f9c78c07b4fcade4095b8aba84595c9b2b19c59f
Adam Tolbert <aotolbert@gmail.com>
2020-07-22 20:35:27 -0400

    its alive

---
+++ routes/routes.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const express = require('express');
+var path = require("path");
+//var sendEmail = require("../functions/send-contact-email");
+// const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+
+const router = express.Router();
+
+
+// Set default caching headers
+router.use((req, res, next) => {
+    res.set('Cache-Control', 'no-cache');
+    next();
+  });
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../app/index.html"));
+})
+
+router.get("/upcoming", (req, res) => {
+    res.sendFile(path.join(__dirname, "../app/index.html"));
+})
+
+router.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "../app/index.html"));
+})
+
+router.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "../app/index.html"));
+})
+
+
+
+
+
+
+// router.get('/sw.js', (req, res) => {
+//     console.log(__dirname)
+// const input = fs.createReadStream(`${__dirname}/../app/sw.js`);
+
+// const toCache = [
+//   './index.html',
+//   './offline.html',
+//   './styles.css',
+//   './debatinator.js'
+// ]
+
+// res.set('Cache-Control', 'public, max-age=31557600'); // one year
+// res.set('Content-Type', 'application/javascript');
+//     input.pipe(res);
+// });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('mz/fs');
 var bodyParser = require("body-parser");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4040;
 
 const app = express();
 
@@ -14,3 +14,5 @@ app.use(express.static(publicDir));
 
 var routes = require("./routes/routes.js");
 app.use(routes);
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
