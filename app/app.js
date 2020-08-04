233c2088229a14678792fb9cafa8ee1d89ef9f92
Adam Tolbert <aotolbert@gmail.com>
2020-08-03 20:15:31 -0400

    mobile scroll not desktop scroll

--- app/app.js
+++ app/app.js
@@ -13,13 +13,15 @@ let maxScollPositionY = scrollHeight - windowHeight;
 console.log('Max scroll => "'+ maxScollPositionY);
 
 function transformBackground(scrollPosition) {
+    ticking = false;
+
     // Because the background image is 125vh we have 25vh "hidden" under the viewport when the user is at the top of the document.
     let degreeOfTransform = (scrollPosition / maxScollPositionY);
     console.log(degreeOfTransform);
 
     let transformValue = degreeOfTransform * -20;
 
-    let viewportTransformString = transformValue + 'px';
+    let viewportTransformString = transformValue + 'vh';
 
     let debugEl = document.getElementById('debuglog');
     debugEl.innerText = viewportTransformString;
@@ -32,6 +34,8 @@ function transformBackground(scrollPosition) {
 }
 
 function transformBackgroundMobile(scrollPosition) {
+    ticking = false;
+
     // Because the background image is 125vh we have 25vh "hidden" under the viewport when the user is at the top of the document.
     let degreeOfTransform = (scrollPosition / maxScollPositionY);
     console.log(degreeOfTransform);
@@ -44,27 +48,50 @@ function transformBackgroundMobile(scrollPosition) {
     console.log(transformValueActualPx);
 
     let viewportTransformString = transformValueActualPx  + 'px';
+    // TODO 'Scroll' the background manipulating the y coordinate proportionate to the amount scrolled / total available scroll space with a max shift value of 25vh
+    body.style.backgroundPositionY = viewportTransformString;
 
     let debugEl = document.getElementById('debuglog');
     debugEl.innerText = viewportTransformString;
 
 
-    // TODO 'Scroll' the background manipulating the y coordinate proportionate to the amount scrolled / total available scroll space with a max shift value of 25vh
-    body.style.backgroundPositionY = viewportTransformString;
     console.log(scrollPosition);
 
 }
 
+var isMobile = navigator.userAgent.match(
+    /(iPhone|iPod|iPad|Android|webOS|BlackBerry|IEMobile|Opera Mini)/i);
 
-window.addEventListener('scroll', function(e) {
-  lastKnownScrollPosition = window.scrollY;
+if(isMobile) {
+    console.log('I am on mobile');
+    window.addEventListener('scroll', function(e) {
+        lastKnownScrollPosition = window.scrollY;
+      
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                transformBackgroundMobile(lastKnownScrollPosition);
+            });
+            
+            ticking = true;
 
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-        transformBackgroundMobile(lastKnownScrollPosition);
-      ticking = false;
-    });
+          }
+          
+      });
+} else {
+    console.log('I am  not on mobile');
 
-    ticking = true;
-  }
-});
+    window.addEventListener('scroll', function(e) {
+        lastKnownScrollPosition = window.scrollY;
+      
+        if (!ticking) {
+          window.requestAnimationFrame(function() {
+            transformBackground(lastKnownScrollPosition);
+            ticking = false;
+          });
+      
+          ticking = true;
+        }
+      });
+}
+
+
